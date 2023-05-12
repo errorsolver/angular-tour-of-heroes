10f26db2932b50820163bf9ec592560c5becd3d9
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2023-05-12 14:58:31 +0700

    Menambahkan Navigasi

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
@@ -15,5 +17,10 @@ export class HeroService {
     return heroes;
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero>{
+    // error handling later
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
